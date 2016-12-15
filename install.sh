a5f8999a2a7a7b4c7fad6ec1383bdde567c43fe4
Geikiy <geikiy@qq.com>
2016-12-15 14:13:57 +0800

    add mysql 5.7 install script

--- install.sh
+++ install.sh
@@ -7,12 +7,19 @@ rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
 rpm -e --nodeps mysql-libs-5.1.73-5.el6_6.x86_64
 
 # from this url to download the package 
-# http://repo.mysql.com/yum/mysql-5.6-community/el/6/x86_64/
+# Version: 5.6 http://repo.mysql.com/yum/mysql-5.6-community/el/6/x86_64/
 rpm -Uvh --force mysql-community-common-5.6.35-2.el6.x86_64.rpm
-rpm -Uvh --force mysql-community-client-5.6.35-2.el6.x86_64.rpm
 rpm -Uvh --force mysql-community-libs-5.6.35-2.el6.x86_64.rpm
 rpm -Uvh --force mysql-community-libs-compat-5.6.35-2.el6.x86_64.rpm
+rpm -Uvh --force mysql-community-client-5.6.35-2.el6.x86_64.rpm
 rpm -Uvh --force mysql-community-server-5.6.35-2.el6.x86_64.rpm
+
+# Version: 5.7 http://repo.mysql.com/yum/mysql-5.7-community/el/6/x86_64/
+rpm -Uvh --force mysql-community-common-5.7.17-1.el6.x86_64.rpm
+rpm -Uvh --force mysql-community-libs-5.7.17-1.el6.x86_64.rpm
+rpm -Uvh --force mysql-community-libs-compat-5.7.17-1.el6.x86_64.rpm
+rpm -Uvh --force mysql-community-client-5.7.17-1.el6.x86_64.rpm
+rpm -Uvh --force mysql-community-server-5.7.17-1.el6.x86_64.rpm
 
 service mysqld start
 
